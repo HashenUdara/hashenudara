21eae1a751a7c7472a2bb6a22ed141fccf4ab05c
Hashen <emhashenudara@gmail.com>
2024-11-30 11:51:00 +0530

    Update codebyhash.js

--- dist/cdn/codebyhash.js
+++ dist/cdn/codebyhash.js
@@ -1,2 +1,2 @@
 const copyright = document.querySelector('#footer-credit');
-copyright.innerHTML = 'Crafted with Passion by <a href="https://github.com/HashenUdara/">Hashen </a>';
+copyright.innerHTML = 'Crafted with Passion by <a href="https://www.linkedin.com/in/hesanda-liyanage/" target="_blank">Hesanda </a>';
