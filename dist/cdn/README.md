b65e3cd4aa25bc4701284f8cf0c32108b49a577d
Hashen <emhashenudara@gmail.com>
2025-07-05 17:53:55 +0530

    Delete dist/cdn directory

--- dist/cdn/README.md
+++
@@ -1,3 +0,0 @@
-CDN:
-
-`https://cdn.jsdelivr.net/gh/HashenUdara/hashenudara@main/dist/cdn/codebyhash.js`
--- dist/cdn/codebyhash.js
+++
@@ -1,2 +0,0 @@
-const copyright = document.querySelector('#footer-credit');
-copyright.innerHTML = 'Crafted with Passion by <a href="https://www.linkedin.com/in/hesanda-liyanage/" target="_blank">Hesanda </a>';
